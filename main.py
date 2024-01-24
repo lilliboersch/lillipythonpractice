bda0298d5eaf0af65b8524c1517d07c8fd658197
Lilli <boerschlilli5@gmail.com>
2024-01-24 12:34:05 +0100

    First commit!

---
+++ main.py
@@ -0,0 +1,14 @@
+print("hello world again!")
+print("hello world again! this is a new change")
+print(5/5)
+print(10//3)
+print(10%3)
+
+print("I am Lilli")
+
+
+number = 10
+if number > 5:
+    print("Number is greater than 5")
+else:
+    print("Number is less than or equal to 5")
